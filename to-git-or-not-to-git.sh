874d301d002f3df2d2951180883a9a08fc2c1d89
ivossos <Johnvossos2001@gmail.com>
2024-09-02 13:03:21 +0300

    To git or not to git

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,5 +1,10 @@
-name="Chameleon"
-power="67"
-gender="Male"
+
+json_url="https://platform.zone01.gr/assets/superhero/all.json"
+
+superhero_info=$(curl -s "$json_url" | jq -r '.[] | select(.id == 170) | {name: .name, power: .powerstats.power, gender: .appearance.gender}')
+
+name=$(jq -r '.name' <<< "$superhero_info")
+power=$(jq -r '.power' <<< "$superhero_info")
+gender=$(jq -r '.gender' <<< "$superhero_info")
 
 printf "%s\n%s\n%s\n" "$name" "$power" "$gender"
