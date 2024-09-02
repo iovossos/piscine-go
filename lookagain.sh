59fc07ca9ed4fd3f007f57d395c3949b63e752a6
ivossos <Johnvossos2001@gmail.com>
2024-09-02 16:46:48 +0300

    Lookin' again

--- lookagain.sh
+++ lookagain.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Find all .sh files, remove the .sh extension, sort them in descending order, and print the result
+# Find all .sh files, remove the .sh extension and the leading ./, sort them in descending order, and print the result
 find . -type f -name "*.sh" | \
-    sed 's/\.sh$//' | \
-    awk '!seen[$0]++' | \
+    sed -e 's/^\.\///' -e 's/\.sh$//' | \
     sort -r
