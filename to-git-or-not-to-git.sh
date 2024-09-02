d7cc7aecb6418088c488d613895c5af224a30189
ivossos <Johnvossos2001@gmail.com>
2024-09-02 12:47:19 +0300

    To git or not to git

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,3 +1,10 @@
-printf "Electro\n"
-printf "67\n"
-printf "Male\n"
+
+json_url="https://platform.zone01.gr/assets/superhero/all.json"
+
+superhero_info=$(curl -s "$json_url" | jq -r '.[] | select(.id == 170) | {name: .name, power: .power, gender: .gender}')
+
+name=$(echo "$superhero_info" | jq -r '.name')
+power=$(echo "$superhero_info" | jq -r '.power')
+gender=$(echo "$superhero_info" | jq -r '.gender')
+
+printf "Name: %s\nPower: %s\nGender: %s\n" "$name" "$power" "$gender"
