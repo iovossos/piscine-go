0b2fd1d88440b5c50b9d45f9f522e8fa09ffecb3
ivossos <johnvossos2001@gmail.com>
2024-09-04 13:09:40 +0300

    PrintComb2

--- printcomb2.go
+++ printcomb2.go
@@ -1,31 +1,32 @@
-package piscine
+package main
 
 import (
 	"github.com/01-edu/z01"
 )
 
-// PrintComb2 prints all possible combinations of two different two-digit numbers
-// in ascending order, with each combination separated by a comma and a space.
+// PrintComb2 prints all unique combinations of two different two-digit numbers.
 func PrintComb2() {
-	for i := 10; i <= 98; i++ {
+	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			printTwoDigitNumber(i)
-			printTwoDigitNumber(j)
+			printTwoDigit(i)
+			z01.PrintRune(' ')
+			printTwoDigit(j)
+			
 			if !(i == 98 && j == 99) {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 	}
-	z01.PrintRune('\n') // Print a newline at the end
+	z01.PrintRune('\n') // Print newline at the end
 }
 
-// printTwoDigitNumber prints a two-digit number using z01.PrintRune
-func printTwoDigitNumber(n int) {
-	// Print the tens place
-	z01.PrintRune(rune(n/10 + '0'))
-	// Print the units place
-	z01.PrintRune(rune(n%10 + '0'))
+// Helper function to print a two-digit number
+func printTwoDigit(n int) {
+	// Print tens digit
+	z01.PrintRune(rune('0' + n/10))
+	// Print units digit
+	z01.PrintRune(rune('0' + n%10))
 }
 
 func main() {
