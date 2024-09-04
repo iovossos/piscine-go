5f31294eb66740fefdde5b20e9f0d517eeba07ac
ivossos <johnvossos2001@gmail.com>
2024-09-04 14:14:01 +0300

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -4,20 +4,19 @@ import (
 	"github.com/01-edu/z01"
 )
 
-// PrintNbr prints the integer n as a string using z01.PrintRune.
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
-		printNumber(-n)
-	} else {
-		printNumber(n)
+		if n == (-9223372036854775808) {
+			PrintNbr(922337203685477580)
+			z01.PrintRune('8')
+			return
+		}
+		n = -n
 	}
-}
 
-// printNumber is a helper function that prints a non-negative integer.
-func printNumber(n int) {
 	if n >= 10 {
-		printNumber(n / 10) // Recursively process the higher digits
+		PrintNbr(n / 10)
 	}
-	z01.PrintRune(rune('0' + n%10)) // Print the current digit
+	z01.PrintRune(n%10 + '0')
 }
