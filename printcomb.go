be79065650fb49c6c0e4ace5be47aa5470697705
ivossos <johnvossos2001@gmail.com>
2024-09-04 12:54:49 +0300

    Owie

--- printcomb.go
+++ printcomb.go
@@ -5,21 +5,21 @@ import (
 )
 
 func printcomb() {
-// Loop through all possible combinations of three different digits
-for i := '0'; i <= '7'; i++ {
-	for j := i + 1; j <= '8'; j++ {
-		for k := j + 1; k <= '9'; k++ {
-			z01.PrintRune(i)
-			z01.PrintRune(j)
-			z01.PrintRune(k)
-			
-			// Check if it is not the last combination
-			if !(i == '7' && j == '8' && k == '9') {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+	// Loop through all possible combinations of three different digits
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+
+				// Check if it is not the last combination
+				if !(i == '7' && j == '8' && k == '9') {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
 			}
 		}
 	}
-}
-z01.PrintRune('\n') // Print a newline at the end
+	z01.PrintRune('\n') // Print a newline at the end
 }
