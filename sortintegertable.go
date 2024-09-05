cafef6cc121ef882f7327cda7aa9834443ffccf8
ivossos <johnvossos2001@gmail.com>
2024-09-05 18:43:08 +0300

    okk

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,7 +1,12 @@
 package piscine
 
-import "sort"
-
 func SortIntegerTable(table []int) {
-	sort.Ints(table)
+	n := len(table)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if table[j] > table[j+1] {
+				table[j], table[j+1] = table[j+1], table[j]
+			}
+		}
+	}
 }
