32df27f7a173d5d2fc835e3880063fba443112f2
ivossos <johnvossos2001@gmail.com>
2024-09-05 17:28:00 +0300

    oof

--- strrev.go
+++ strrev.go
@@ -3,7 +3,7 @@ package piscine
 func StrRev(s string) string {
 	rns := []rune(s)
 	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
-		rns[i], rns[j] = rns[i], rns[i]
+		rns[j], rns[j] = rns[i], rns[i]
 	}
 	return string(rns)
 }
