45ae8c20983c42d9bafcab679dc37bbeff1158b4
ivossos <johnvossos2001@gmail.com>
2024-09-09 20:45:55 +0300

    Huh

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,12 +1,18 @@
 package piscine
 
-func RecursiveFactorial(nb int) int {
-	// Upper bound check to prevent deep recursion and overflow
-	if nb < 0 || nb > 12 { // 12! is the largest factorial that fits in a 32-bit int
+func RecursiveFactorial(nb int) int64 {
+	if nb < 0 {
 		return 0
 	}
 	if nb == 0 || nb == 1 {
 		return 1
 	}
-	return nb * RecursiveFactorial(nb-1)
+
+	// Prevent overflow for 64-bit integers
+	nextFactorial := RecursiveFactorial(nb - 1)
+	if nb > int((1<<63-1)/nextFactorial) {
+		return 0 // Overflow detected
+	}
+
+	return int64(nb) * nextFactorial
 }
