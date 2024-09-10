0beaa73085a43e564852c3d770c0a6ce6ae39d90
ivossos <johnvossos2001@gmail.com>
2024-09-10 11:41:21 +0300

    Oof

---
+++ printnbrinorder.go
@@ -0,0 +1,29 @@
+package piscinge
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder (n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		z01.PrintRune('\n')
+		return
+	}
+	digits := []int{}
+	for n > 0 {
+		digits = append(digits, n%10)
+		n /= 10
+	}
+
+	for i := 0; i < len(digits); i++ {
+		for j := 0; j < len(digits)-i-1; j++ {
+			if digits[j] > digits[j+1] [
+				digits[j], digits[j+1] = digits[j+1], digits[j]
+			]
+		}
+	}
+
+	for _, d := range digits {
+		z01.PrintRune(rune(d + '0'))
+	}
+	z01.PrintRune('\n')
+}
