18ef60007d92cbba82f1c55bad9d3bac0bc59140
ivossos <johnvossos2001@gmail.com>
2024-09-10 12:42:46 +0300

    Cap

---
+++ capitalize.go
@@ -0,0 +1,28 @@
+package piscine
+
+func Capitalize(s string) string {
+	var result []rune
+	shouldUpper := true
+	for _, ch := range s {
+		if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') {
+			if shouldUpper {
+				if ch >= 'a' && ch <= 'z' {
+					ch -= 32
+				}
+				shouldUpper = false
+			} else {
+				if ch >= 'A' && ch <= 'Z' {
+					ch += 32
+				}
+			}
+			result = append(result, ch)
+		} else {
+			if !(ch >= 'a' && ch <= 'z') || !(ch >= 'A' && ch <= 'Z') || !(ch >= '0' && ch <= '9') {
+				shouldUpper = true
+			}
+		}
+	}
+
+	return string(result)
+
+}
