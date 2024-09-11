b0bb49adc55fa8b5a9af9493c9504200e94f3030
ivossos <johnvossos2001@gmail.com>
2024-09-11 14:09:52 +0300

    nbr convert

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -6,55 +6,40 @@ import (
 	"github.com/01-edu/z01"
 )
 
-// Custom atoi function to convert string to integer
-func atoi(s string) int {
-	result := 0
-	for _, char := range s {
-		if char < '0' || char > '9' {
-			// Return 0 for invalid characters (non-digit)
-			return 0
-		}
-		result = result*10 + int(char-'0')
+func printLetter(n int, upper bool) {
+	if n < 1 || n > 26 {
+		z01.PrintRune(' ')
+		return
 	}
-	return result
+	letter := rune(n + 96) // 'a' is at position 97 in Unicode
+	if upper {
+		letter -= 32 // Convert to uppercase
+	}
+	z01.PrintRune(letter)
 }
 
 func main() {
-	// Check if the --upper flag is provided
-	upper := false
-	if len(os.Args) > 1 && os.Args[1] == "--upper" {
-		upper = true
-		os.Args = os.Args[1:] // Skip the flag for further processing
-	}
+	args := os.Args
 
-	// If no arguments are provided, just print a newline and exit
-	if len(os.Args) == 1 {
-		z01.PrintRune('\n')
+	if len(args) < 2 {
 		return
 	}
 
-	// Loop through each argument (excluding the program name)
-	for i := 1; i < len(os.Args); i++ {
-		// Convert the argument to an integer using the custom atoi function
-		n := atoi(os.Args[i])
-		if n < 1 || n > 26 {
-			// Print a space if the argument is not valid or not in the range 1-26
-			z01.PrintRune(' ')
-			continue
-		}
-
-		// Calculate the corresponding letter
-		var letter rune
-		if upper {
-			letter = rune('A' + n - 1) // Uppercase letter
-		} else {
-			letter = rune('a' + n - 1) // Lowercase letter
-		}
-
-		// Print the letter
-		z01.PrintRune(letter)
+	upper := false
+	if args[1] == "--upper" {
+		upper = true
+		args = args[1:]
 	}
 
-	// Print a newline at the end
-	z01.PrintRune('\n')
+	for _, arg := range args[1:] {
+		n := 0
+		for _, ch := range arg {
+			if ch < '0' || ch > '9' {
+				z01.PrintRune(' ')
+				continue
+			}
+			n = n*10 + int(ch-'0')
+		}
+		printLetter(n, upper)
+	}
 }
