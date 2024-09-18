e7c512b811c3bbb783fa6bbd320d555f35a93205
ivossos <johnvossos2001@gmail.com>
2024-09-18 20:17:41 +0300

    oof

---
+++ sortwordarr.go
@@ -0,0 +1,12 @@
+package piscine
+
+// SortWordArr sorts a slice of strings by ASCII in ascending order.
+func SortWordArr(a []string) {
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a)-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
