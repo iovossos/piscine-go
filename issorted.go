177208a2aef5830f1e96637d0b6804d25f349a45
ivossos <johnvossos2001@gmail.com>
2024-09-18 20:08:31 +0300

    Ooof

--- issorted.go
+++ issorted.go
@@ -1,9 +1,9 @@
 package piscine
 
-// IsSorted checks if the slice a is sorted based on the comparison function f.
+// IsSorted checks if the slice a is sorted in ascending order based on the comparison function f.
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 1; i < len(a); i++ {
-		if f(a[i-1], a[i]) > 0 {
+		if f(a[i-1], a[i]) > 0 { // if previous element is greater than current, not sorted
 			return false
 		}
 	}
