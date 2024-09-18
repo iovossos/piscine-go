478e35c7218ed525e90a98a7eaa1f1e4b61425f8
ivossos <johnvossos2001@gmail.com>
2024-09-18 20:18:44 +0300

    oof

---
+++ advancedsortwordarr.go
@@ -0,0 +1,12 @@
+package piscine
+
+// AdvancedSortWordArr sorts a slice of strings based on the comparison function f.
+func AdvancedSortWordArr(a []string, f func(a, b string) int) {
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a)-1; j++ {
+			if f(a[j], a[j+1]) > 0 {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
