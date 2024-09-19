2324c6b9b917d8a436edea6cf7437af0f8cc196c
ivossos <johnvossos2001@gmail.com>
2024-09-19 22:09:27 +0300

    oof

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	tempA := ***a
+	tempC := *******c
+	tempD := ****d
+	tempB := *b
+
+	***a = tempB
+	*b = tempD
+	****d = tempC
+	*******c = tempA
+}
