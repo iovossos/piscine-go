fdf88e0a725f68469754b0d22382689d139ea09f
ivossos <johnvossos2001@gmail.com>
2024-09-19 11:13:05 +0300

    rot14

--- descendcomb.go
+++ descendcomb.go
@@ -1,8 +1,6 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func DescendComb() {
 	for i := 99; i >= 0; i-- {
@@ -10,7 +8,7 @@ func DescendComb() {
 			printTwoDigits(i)
 			z01.PrintRune(' ')
 			printTwoDigits(j)
-			if i != 1 || j != 0 {
+			if !(i == 1 && j == 0) { // To avoid the trailing comma and space
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
@@ -20,15 +18,8 @@ func DescendComb() {
 }
 
 func printTwoDigits(n int) {
-	if n < 10 {
-		z01.PrintRune('0')
-	}
-	printNumber(n)
-}
-
-func printNumber(n int) {
-	if n >= 10 {
-		z01.PrintRune(rune(n/10 + '0'))
-	}
+	// Print the first digit (tens place)
+	z01.PrintRune(rune(n/10 + '0'))
+	// Print the second digit (ones place)
 	z01.PrintRune(rune(n%10 + '0'))
 }
