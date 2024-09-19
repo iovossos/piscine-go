49442e170a9cee5b55c7c5fd93387e7ae7d444b8
ivossos <johnvossos2001@gmail.com>
2024-09-19 23:20:26 +0300

    oof

--- descendappendrange.go
+++ descendappendrange.go
@@ -2,11 +2,11 @@ package piscine
 
 func DescendAppendRange(max, min int) []int {
 	if max <= min {
-		return nil // Return nil slice if min is greater than or equal to max
+		return []int{} // Return nil slice if min is greater than or equal to max
 	}
 
 	// Initialize an empty slice (without using make)
-	var result []int
+	result := []int{}
 
 	// Append values from min to max - 1
 	for i := max; i > min; i-- {
