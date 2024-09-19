5a16f95dcc33a890267c727d61a73f4eea27f8e6
ivossos <johnvossos2001@gmail.com>
2024-09-20 00:09:51 +0300

    oof

--- shoppinglistsort.go
+++ shoppinglistsort.go
@@ -1,12 +1,12 @@
 package piscine
 
 func ShoppingListSort(slice []string) []string {
-	for i := 0; i len(slice); i++ {
-		for j := 0; j< len(slice)-1-i; j++{
-			if len(slice[j]) > len(slice[j+1]){
+	for i := 0; i < len(slice); i++ {
+		for j := 0; j < len(slice)-1-i; j++ {
+			if len(slice[j]) > len(slice[j+1]) {
 				slice[j], slice[j+1] = slice[j+1], slice[j]
 			}
 		}
 	}
-	return slice 
+	return slice
 }
