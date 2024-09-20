c5828c33b7a2c71f7955f23b2bdc88c3639846e9
ivossos <johnvossos2001@gmail.com>
2024-09-20 07:02:41 +0300

    oof

--- fixthemain/main.go
+++ fixthemain/main.go
@@ -6,50 +6,47 @@ type Door struct {
 	state bool
 }
 
-const (
-	OPEN  = false
-	CLOSE = true
-)
-
-// PrintStr prints a string using z01.PrintRune
-func PrintStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
+func PrintStr(str string) {
+	arrayRune := []rune(str)
+	for _, s := range arrayRune {
+		z01.PrintRune(s)
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
 
-func OpenDoor(ptrDoor *Door) {
+func OpenDoor(ptrDoor *Door) bool {
 	PrintStr("Door Opening...")
-	ptrDoor.state = OPEN
+	ptrDoor.state = true
+	return true
 }
 
-func CloseDoor(ptrDoor *Door) {
-	PrintStr("Door closing...")
-	ptrDoor.state = CLOSE
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...")
+	ptrDoor.state = false
+	return true
 }
 
-func IsDoorOpen(ptrDoor Door) bool {
-	PrintStr("Door is open ?")
-	return ptrDoor.state
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("is the Door opened ?")
+	return ptrDoor.state == true
 }
 
-func IsDoorClose(ptrDoor Door) bool {
-	PrintStr("Door is close ?")
-	return ptrDoor.state
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	return ptrDoor.state == false
 }
 
 func main() {
-	door := Door{}
+	door := &Door{}
 
-	OpenDoor(&door)
+	OpenDoor(door)
 	if IsDoorClose(door) {
-		OpenDoor(&door)
+		OpenDoor(door)
 	}
 	if IsDoorOpen(door) {
-		CloseDoor(&door)
+		CloseDoor(door)
 	}
-	if door.state == OPEN {
-		CloseDoor(&door)
+	if door.state == true {
+		CloseDoor(door)
 	}
 }
