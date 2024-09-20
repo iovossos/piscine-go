b6d98924ab5d954c52f1817ee72789ac015711d3
ivossos <johnvossos2001@gmail.com>
2024-09-20 09:49:43 +0300

    sdsd

--- loafofbread.go
+++ loafofbread.go
@@ -6,7 +6,7 @@ func LoafOfBread(str string) string {
 
 	for i := 0; i < len(str); i++ {
 		if str[i] == ' ' {
-			result += " " // Add space directly to the result
+			result += " " // Add spaces to the result but don't count them
 			continue
 		}
 
@@ -14,14 +14,21 @@ func LoafOfBread(str string) string {
 			result += string(str[i]) // Add non-space characters to the result
 			count++
 		} else if count == 5 {
-			// Skip the 6th character and reset count
+			// Skip the 6th non-space character and reset the count
 			count = 0
 			continue
 		}
 	}
 
-	// If the total number of non-space characters is less than 5, return "Invalid Output\n"
-	if count > 0 && count < 5 {
+	// If there are fewer than 5 non-space characters, return "Invalid Output\n"
+	nonSpaceCount := 0
+	for i := 0; i < len(result); i++ {
+		if result[i] != ' ' {
+			nonSpaceCount++
+		}
+	}
+
+	if nonSpaceCount < 5 {
 		return "Invalid Output\n"
 	}
 
