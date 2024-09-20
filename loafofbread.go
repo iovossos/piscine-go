b48ea22a754c6bcf0286b0147531b8c8f3564f73
ivossos <johnvossos2001@gmail.com>
2024-09-20 09:35:10 +0300

    oof

--- loafofbread.go
+++ loafofbread.go
@@ -2,25 +2,29 @@ package piscine
 
 func LoafOfBread(str string) string {
 	var result string
-	var count int // To count characters excluding spaces
+	var wordCount int // To count characters excluding the skipped ones
+	var charCount int // To count all characters, including spaces
 
 	for i := 0; i < len(str); i++ {
-		if str[i] == ' ' { // Ignore spaces
+		if str[i] == ' ' { // Include spaces in the result
+			result += " "
 			continue
 		}
 
-		if count < 5 {
+		if wordCount < 5 {
 			result += string(str[i]) // Add characters to the result until 5 chars are collected
-			count++
-		} else if count == 5 {
-			// Skip the 6th character
-			count = 0
+			wordCount++
+		} else if wordCount == 5 {
+			// Skip the 6th character and reset the word count
+			wordCount = 0
 			continue
 		}
+
+		charCount++
 	}
 
 	// If the string has less than 5 characters, return "Invalid Output\n"
-	if len(result) < 5 {
+	if charCount < 5 {
 		return "Invalid Output\n"
 	}
 
