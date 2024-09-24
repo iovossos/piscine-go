6e555f0220607aa6899c3504c1e0c4417e57a875
ivossos <johnvossos2001@gmail.com>
2024-09-24 16:17:55 +0300

    Oof

---
+++ listpushfront.go
@@ -0,0 +1,23 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	newNode := &NodeL{Data: data}
+
+	newNode.Next = l.Head
+
+	l.Head = newNode
+
+	if l.Tail == nil {
+		l.Tail = newNode
+	}
+}
