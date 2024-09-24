c7c7673c21dc5011ac5f9f155d04879624c59add
ivossos <johnvossos2001@gmail.com>
2024-09-24 16:30:08 +0300

    Oof

---
+++ listlast.go
@@ -0,0 +1,14 @@
+package piscine
+
+func ListLast(l *List) interface{} {
+	if l.Head == nil {
+		return nil
+	}
+	node := l.Head
+
+	for node.Next != nil {
+		node = node.Next
+	}
+
+	return node.Data
+}
